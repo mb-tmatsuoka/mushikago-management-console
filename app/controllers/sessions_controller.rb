3f64e28ce3458c204d8930a7907c3bbc7306e94b
Toru Matsuoka <t.matsuoka@miningbrownie.co.jp>
2012-08-20 12:43:20 +0900

    Added redirect to projects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,6 @@ class SessionsController < ApplicationController
   def create
     session[:api_key] = request[:api_key]
     session[:secret_key] = request[:secret_key]
+    redirect_to '/projects/index'
   end
 end
