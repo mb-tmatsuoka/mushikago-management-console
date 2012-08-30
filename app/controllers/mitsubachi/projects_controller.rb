d02938fd8a61d5458b25d1a82fb01a0ce0efa2c1
Toru Matsuoka <t.matsuoka@miningbrownie.co.jp>
2012-08-30 13:03:12 +0900

    Added show projects

--- app/controllers/mitsubachi/projects_controller.rb
+++ app/controllers/mitsubachi/projects_controller.rb
@@ -2,4 +2,9 @@ class Mitsubachi::ProjectsController < ApplicationController
   def index
     @projects = mitsubachi.project_list
   end
+
+  def show
+    @resources = mitsubachi.resource_list(request[:id])
+    @scripts = mitsubachi.script_list(request[:id])
+  end
 end
